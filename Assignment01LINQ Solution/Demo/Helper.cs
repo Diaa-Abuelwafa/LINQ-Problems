a4a55f3d55f056f7306ba8b8de5380b5acfc214f
Diaa Abuelwafa <diaaabuelwafa@gmail.com>
2024-08-16 07:50:47 +0200

    Initial Commit

---
+++ Assignment01LINQ Solution/Demo/Helper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal static class Helper
+    {
+        public static void Print(this int A)
+        {
+            Console.WriteLine($"Variable Value = {A}");
+        }
+    }
+}
